09d112a10a520e24a2b80d984551e72cc5e72e59
Dagson <dagsonmg@yahoo.com.br>
2023-04-30 17:19:22 -0300

    Ajustes Finais para Deploy

--- src/UI/components/inputs/UserForm/forms/FiancialForm.tsx
+++ src/UI/components/inputs/UserForm/forms/FiancialForm.tsx
@@ -1,19 +1,24 @@
 import { FormValues } from "data/@types/forms/FormValue";
+import { UserContext } from "data/contexts/UserContext";
+import { useContext } from "react";
 import { useFormContext } from "react-hook-form";
 import TextField from "UI/components/inputs/TextField/TextField";
 import { FinancialData } from "../UserForm.styled";
 
-const FinalcialForm = () => {
-  const { register } = useFormContext<FormValues>();
+const FinancialForm = () => {
+  const { register } = useFormContext<FormValues>(),
+    {
+      userState: { user },
+    } = useContext(UserContext);
   return (
     <FinancialData>
       <TextField
         label={"Chave Pix"}
-        defaultValue={""}
+        defaultValue={user.chave_pix}
         {...register("usuario.chave_pix", { minLength: 5 })}
       />
     </FinancialData>
   );
 };
 
-export default FinalcialForm;
+export default FinancialForm;
